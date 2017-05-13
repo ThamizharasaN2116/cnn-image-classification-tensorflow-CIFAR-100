7aac47461149bd064e4629ae6aad0275dd389391
ThamizharasaN <thamizh.cool21@gmail.com>
2017-05-13 11:21:05 +0530

    First Commit

---
+++ helper.py
@@ -0,0 +1,267 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
+
+
+def _load_label_names():
+    """
+    Load the label names from file
+    """
+    return ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+def _load_label_name_100():
+    """
+    Load the label names from file
+    """
+    return ["beaver", " dolphin", " otter", " seal", " whale", " aquarium fish", " flatfish", " ray", " shark", " trout", " orchids", " poppies", " roses", " sunflowers", " tulips", " bottles", " bowls", " cans", 
+            " cups", " plates", " apples", " mushrooms", " oranges", " pears", " sweet peppers", " clock", " computer keyboard", " lamp", " telephone", " television", " bed", " chair", " couch", " table", " wardrobe", " bee", 
+            " beetle", " butterfly", " caterpillar", " cockroach", " bear", " leopard", " lion", " tiger", " wolf", " bridge", " castle", " house", " road", " skyscraper", " cloud", " forest", " mountain", " plain", " sea", 
+            " camel", " cattle", " chimpanzee", " elephant", " kangaroo", " fox", " porcupine", " possum", " raccoon", " skunk", " crab", " lobster", " snail", " spider", " worm", " baby", " boy", " girl", " man", " woman", 
+            " crocodile", " dinosaur", " lizard", " snake", " turtle", " hamster", 
+            " mouse", " rabbit", " shrew", " squirrel", " maple", " oak", " palm", " pine", " willow", " bicycle", " bus", " motorcycle", " pickup truck", " train", " lawn-mower", " rocket", " streetcar", " tank", " tractor"]
+
+    # picked from 
+    # import pickle
+    # with open(cifar100_dataset_folder_path+"/meta", 'rb') as fo:
+    #     dict = pickle.load(fo, encoding='latin')
+    # print(dict['fine_label_names'])
+    #return ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
+
+
+
+def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
+    """
+    Load a batch of the dataset
+    """
+    with open(cifar10_dataset_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
+        batch = pickle.load(file, encoding='latin1')
+
+    features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+    labels = batch['labels']
+
+    return features, labels
+
+def load_cfar100_batch(cifar10_dataset_folder_path):
+    """
+    Load a batch of the dataset
+    """
+    with open(cifar10_dataset_folder_path + '/train', mode='rb') as file:
+        batch = pickle.load(file, encoding='latin1')
+
+    features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+    labels = batch['fine_labels']
+    
+    return features, labels
+
+def display_stats(cifar10_dataset_folder_path, batch_id, sample_id):
+    """
+    Display Stats of the the dataset
+    """
+    label_names = None
+    if cifar10_dataset_folder_path == 'cifar-100-python':
+        print('x')
+        features, labels = load_cfar100_batch(cifar10_dataset_folder_path)
+        label_names = _load_label_name_100()
+    else:
+        batch_ids = list(range(1, 6))
+        if batch_id not in batch_ids:
+            print('Batch Id out of Range. Possible Batch Ids: {}'.format(batch_ids))
+            return None
+        features, labels = load_cfar10_batch(cifar10_dataset_folder_path, batch_id)
+        label_names = _load_label_names()
+        
+
+    if not (0 <= sample_id < len(features)):
+        print('{} samples in batch {}.  {} is out of range.'.format(len(features), batch_id, sample_id))
+        return None
+
+    print('\nStats of batch {}:'.format(batch_id))
+    print('Samples: {}'.format(len(features)))
+    print('Label Counts: {}'.format(dict(zip(*np.unique(labels, return_counts=True)))))
+    print('First 20 Labels: {}'.format(labels[:20]))
+
+    sample_image = features[sample_id]
+    sample_label = labels[sample_id]
+    #print(labels)
+    print('sample label',sample_label)
+
+    print('\nExample of Image {}:'.format(sample_id))
+    print('Image - Min Value: {} Max Value: {}'.format(sample_image.min(), sample_image.max()))
+    print('Image - Shape: {}'.format(sample_image.shape))
+    print('Label - Label Id: {} Name: {}'.format(sample_label, label_names[sample_label]))
+    plt.axis('off')
+    plt.imshow(sample_image)
+
+
+def _preprocess_and_save(normalize, one_hot_encode, features, labels, filename):
+    """
+    Preprocess data and save it to file
+    """
+    features = normalize(features)
+    labels = one_hot_encode(labels)
+
+    pickle.dump((features, labels), open(filename, 'wb'))
+
+
+def preprocess_and_save_data_10(cifar10_dataset_folder_path, normalize, one_hot_encode):
+    """
+    Preprocess Training and Validation Data
+    """
+    n_batches = 5
+    valid_features = []
+    valid_labels = []
+
+    for batch_i in range(1, n_batches + 1):
+        features, labels = load_cfar10_batch(cifar10_dataset_folder_path, batch_i)
+        validation_count = int(len(features) * 0.1)
+
+        # Prprocess and save a batch of training data
+        _preprocess_and_save(
+            normalize,
+            one_hot_encode,
+            features[:-validation_count],
+            labels[:-validation_count],
+            'preprocess_batch_' + str(batch_i) + '.p')
+
+        # Use a portion of training batch for validation
+        valid_features.extend(features[-validation_count:])
+        valid_labels.extend(labels[-validation_count:])
+
+    # Preprocess and Save all validation data
+    _preprocess_and_save(
+        normalize,
+        one_hot_encode,
+        np.array(valid_features),
+        np.array(valid_labels),
+        'preprocess_validation.p')
+
+    with open(cifar10_dataset_folder_path + '/test_batch', mode='rb') as file:
+        batch = pickle.load(file, encoding='latin1')
+
+    # load the test data
+    test_features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+    test_labels = batch['labels']
+
+    # Preprocess and Save all test data
+    _preprocess_and_save(
+        normalize,
+        one_hot_encode,
+        np.array(test_features),
+        np.array(test_labels),
+        'preprocess_test.p')
+
+def preprocess_and_save_data_100(cifar100_dataset_folder_path, normalize, one_hot_encode):
+    """
+    Preprocess Training and Validation Data
+    """
+    n_batches = 5
+    valid_features = []
+    valid_labels = []
+
+    features, labels = load_cfar100_batch(cifar100_dataset_folder_path)
+    validation_count = int(len(features) * 0.1)
+
+    # Prprocess and save a batch of training data
+    _preprocess_and_save(
+        normalize,
+        one_hot_encode,
+        features[:-validation_count],
+        labels[:-validation_count],
+        'preprocess_train_100.p')
+
+    # Use a portion of training batch for validation
+    valid_features.extend(features[-validation_count:])
+    valid_labels.extend(labels[-validation_count:])
+        
+
+    # Preprocess and Save all validation data
+    _preprocess_and_save(
+        normalize,
+        one_hot_encode,
+        np.array(valid_features),
+        np.array(valid_labels),
+        'preprocess_validation_100.p')
+
+    with open(cifar100_dataset_folder_path + '/test', mode='rb') as file:
+        batch = pickle.load(file, encoding='latin1')
+
+    # load the test data
+    test_features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+    test_labels = batch['fine_labels']
+
+    # Preprocess and Save all test data
+    _preprocess_and_save(
+        normalize,
+        one_hot_encode,
+        np.array(test_features),
+        np.array(test_labels),
+        'preprocess_test_100.p')
+
+def preprocess_and_save_data(folder_path, normalize, one_hot_encode):
+    """
+    Preprocess Training and Validation Data
+    """
+    if folder_path == 'cifar-10-batches-py':
+        preprocess_and_save_data_10(folder_path, normalize, one_hot_encode)
+    else:
+        preprocess_and_save_data_100(folder_path, normalize, one_hot_encode)
+
+def batch_features_labels(features, labels, batch_size):
+    """
+    Split features and labels into batches
+    """
+    for start in range(0, len(features), batch_size):
+        end = min(start + batch_size, len(features))
+        yield features[start:end], labels[start:end]
+
+
+def load_preprocess_training_batch(batch_id, batch_size):
+    """
+    Load the Preprocessed Training data and return them in batches of <batch_size> or less
+    """
+    filename = 'preprocess_batch_' + str(batch_id) + '.p'
+    features, labels = pickle.load(open(filename, mode='rb'))
+
+    # Return the training data in batches of size <batch_size> or less
+    return batch_features_labels(features, labels, batch_size)
+
+def load_preprocess_training_100(batch_id, batch_size):
+    """
+    Load the Preprocessed Training data and return them in batches of <batch_size> or less
+    """
+    filename = 'preprocess_train_100.p'
+    features, labels = pickle.load(open(filename, mode='rb'))
+
+    # Return the training data in batches of size <batch_size> or less
+    return batch_features_labels(features, labels, batch_size)
+
+def display_image_predictions(features, labels, predictions):
+    n_classes = 10
+    label_names = _load_label_names()
+    label_binarizer = LabelBinarizer()
+    label_binarizer.fit(range(n_classes))
+    label_ids = label_binarizer.inverse_transform(np.array(labels))
+
+    fig, axies = plt.subplots(nrows=4, ncols=2)
+    fig.tight_layout()
+    fig.suptitle('Softmax Predictions', fontsize=20, y=1.1)
+
+    n_predictions = 3
+    margin = 0.05
+    ind = np.arange(n_predictions)
+    width = (1. - 2. * margin) / n_predictions
+
+    for image_i, (feature, label_id, pred_indicies, pred_values) in enumerate(zip(features, label_ids, predictions.indices, predictions.values)):
+        pred_names = [label_names[pred_i] for pred_i in pred_indicies]
+        correct_name = label_names[label_id]
+
+        axies[image_i][0].imshow(feature)
+        axies[image_i][0].set_title(correct_name)
+        axies[image_i][0].set_axis_off()
+
+        axies[image_i][1].barh(ind + margin, pred_values[::-1], width)
+        axies[image_i][1].set_yticks(ind + margin)
+        axies[image_i][1].set_yticklabels(pred_names[::-1])
+        axies[image_i][1].set_xticks([0, 0.5, 1.0])
